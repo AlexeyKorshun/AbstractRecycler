dc856b233d566211468300d8de17a9c1a2c87476
Alexey Korshun <lehakorshun@gmail.com>
2019-02-12 17:01:35 +0600

    convert default diff utils to java

---
+++ library/src/main/java/com/rosberry/abstractrecycler/DefaultDiffCallback.java
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.abstractrecycler;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+/**
+ * @author Alexei Korshun on 12/02/2019.
+ */
+public final class DefaultDiffCallback extends DiffUtil.Callback {
+
+    private final List<AbstractItem> oldItems;
+    private final List<AbstractItem> newItems;
+
+    public DefaultDiffCallback(List<AbstractItem> oldItems, List<AbstractItem> newItems) {
+        this.oldItems = oldItems;
+        this.newItems = newItems;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldItems.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newItems.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldPosition, int newPosition) {
+        AbstractItem oldItem = oldItems.get(oldPosition);
+        AbstractItem newItem = oldItems.get(newPosition);
+        return oldItem.equals(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldPosition, int newPosition) {
+        AbstractItem oldItem = oldItems.get(oldPosition);
+        AbstractItem newItem = oldItems.get(newPosition);
+        return oldItem.equals(newItem);
+    }
+}
