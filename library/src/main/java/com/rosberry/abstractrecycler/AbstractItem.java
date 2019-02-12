e7dd6ef1123f185cbf13a5aa86d55ed2e80df1d0
Alexey Korshun <lehakorshun@gmail.com>
2019-02-12 17:05:32 +0600

    refactor item

--- library/src/main/java/com/rosberry/abstractrecycler/AbstractItem.java
+++ library/src/main/java/com/rosberry/abstractrecycler/AbstractItem.java
@@ -8,19 +8,44 @@ package com.rosberry.abstractrecycler;
 
 import android.support.annotation.LayoutRes;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 /**
  * @author Alexei Korshun on 12/02/2019.
  */
 public class AbstractItem {
 
-    private final Integer type;
+    private final int type;
 
     public AbstractItem(@LayoutRes Integer type) {
         this.type = type;
     }
 
     @LayoutRes
-    public Integer getType() {
+    public int getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractItem that = (AbstractItem) o;
+        return Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "AbstractItem{" +
+                "type=" + type +
+                '}';
     }
 }
